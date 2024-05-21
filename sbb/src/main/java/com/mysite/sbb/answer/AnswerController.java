a924710544ad1940a0e47ceb05bb6a4ddd9eb0a4
eunji <zoooz0616@gmail.com>
2024-05-21 10:41:10 +0900

    [eunji][#add] 앵커 기능, 마크다운 적용

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -43,9 +43,10 @@ public class AnswerController {
 			model.addAttribute("question",question);
 			return "question_detail";
 		}
-		this.answerService.create(question, answerForm.getContent(), siteUser);
+		Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
 		
-		return String.format("redirect:/question/detail/%s", id);
+		return String.format("redirect:/question/detail/%s#answer_%s", 
+				answer.getQuestion().getId(), answer.getId());
 	}
 	
 	@PreAuthorize("isAuthenticated()")
@@ -71,7 +72,8 @@ public class AnswerController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 업습니다.");
 		}
 		this.answerService.modify(answer, answerForm.getContent());
-		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+		return String.format("redirect:/question/detail/%s#answer_%s", 
+				answer.getQuestion().getId(), answer.getId());
 	}
 	
 	@PreAuthorize("isAuthenticated()")
@@ -92,7 +94,8 @@ public class AnswerController {
 		Answer answer = this.answerService.getAnswer(id);
 		SiteUser siteUser = this.userService.getUser(principal.getName());
 		this.answerService.vote(answer, siteUser);
-		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+		return String.format("redirect:/question/detail/%s#answer_%s", 
+				answer.getQuestion().getId(), answer.getId());
 	}
 	
 	
